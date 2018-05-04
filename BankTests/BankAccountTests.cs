e938b7c69575877509f62846a1de9ef702877d5a
Ramiro Bueno Martínez <ramirobmar@gmail.com>
2018-05-04 17:14:54 +0200

    Version 1
    Versión Inicial Demo Pruebas Unitarias en Lenguaje C#

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountNS.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void DebitTest()
+        {
+            BankAccount target = new BankAccount("Mr. Bryan Walton", 11.99);
+            double amount = 11.95;
+            Assert.AreEqual((System.Convert.ToDouble(amount)), target.Balance, 0.05, "Exito");
+        }
+
+        [TestMethod()]
+        public void CreditTest()
+        {
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
